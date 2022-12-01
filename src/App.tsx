d8f15ab31b4662f5c48ab6c95bf92e1acb59b478
Victor Rabello <109052634+VictorRabello97@users.noreply.github.com>
2022-11-30 22:47:21 -0300

    Organizando os arquivos para iniciar a parte de Cardápio

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Body } from "./components/Body"
-import { MainBar } from "./components/Main Bar"
-import { Logo } from "./components/Logo"
+import { Body } from "./components/MainPage/Body"
+import { MainBar } from "./components/MainPage/Main Bar"
+import { Logo } from "./components/MainPage/Logo"
 import { GlobalStyle} from "./global"
-import { Maps } from "./components/GoogleMaps"
-import { Footer } from "./components/Footer"
+import { Maps } from "./components/MainPage/GoogleMaps"
+import { Footer } from "./components/MainPage/Footer"
 
 function App() {
 
